45418606385c47a15445a4dca28e73c632834899
Nathan Glick <nxg9997@g.rit.edu>
2018-01-08 15:33:43 -0500

    started creating physics

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	Physics pPhys;
+
+	// Use this for initialization
+	void Start () {
+		pPhys = GetComponent<Physics> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Movement(){
+		if (Input.GetKey (KeyCode.A) == true) {
+			//pPhys.
+		}
+	}
+}
